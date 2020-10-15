9975991dd613ba56acc75c624c89eca2eb73cda6
Stefan Andersson <stefananderssonoxe@gmail.com>
2020-10-15 10:08:36 +0200

    första

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int aVidstart=10;
+        int bVidStart=20;
+        System.out.println("a ="+aVidstart+" b ="+bVidStart);
+
+        int a = 10;
+        int b = 20;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swaping:"
+                + " a = " + a + ", b = " + b);
+    }
+}
